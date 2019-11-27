3417fce6fc38cd8b4b77d6fef6766be23cf2fc9a
akshaysb100 <akshaybavalekar100@gmail.com>
2019-11-27 17:46:44 +0530

    Add:Create generic method in moodAnalyserReflaction class

--- src/main/java/com/moodanalyser/com/MoodAnalyserReflector.java
+++ src/main/java/com/moodanalyser/com/MoodAnalyserReflector.java
@@ -9,30 +9,6 @@ import java.lang.reflect.InvocationTargetException;
 
 public class MoodAnalyserReflector {
 
-//    public static MoodAnalyser createMoodAnalyser() throws MoodAnalysisException {
-//
-//        try {
-//            Class<?> moodClass = Class.forName("com.moodanalyser.com.MoodAnalyser");
-//            Constructor<?> moodConstructor = null;
-//            moodConstructor = moodClass.getConstructor();
-//            Object newObject = moodConstructor.newInstance();
-//            return (MoodAnalyser) newObject;
-//        } catch (ClassNotFoundException e) {
-//            throw  new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_CLASS,"Class not found");
-//        }catch (NoSuchMethodException e) {
-//            throw  new MoodAnalysisException(MoodAnalysisException.ExceptionType.NO_SUCH_METHOD,"Method not found");
-//
-//        }catch (InstantiationException e) {
-//            e.printStackTrace();
-//        } catch (IllegalAccessException e) {
-//            e.printStackTrace();
-//        } catch (InvocationTargetException e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
-
-
     public static Constructor<?> getConstructor(Class<?> ... param) throws MoodAnalysisException {
 
         try {
